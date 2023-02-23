83f942091ed1625446d299cebaf92a5f0ff878a9
Ashish kumar Sahoo <kumarsahoo912@gmail.com>
2023-02-23 22:08:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 //your code here!
 //solution
+const list = document.getElementById('infi-list');
 
+// Add 10 list items by default
+for (let i = 1; i <= 10; i++) {
+  const listItem = document.createElement('li');
+  listItem.textContent = `List item ${i}`;
+  list.appendChild(listItem);
+}
+
+// Add 2 more list items when the user reaches the end of the list
+list.addEventListener('scroll', () => {
+  const { scrollTop, scrollHeight, clientHeight } = list;
+  if (scrollTop + clientHeight >= scrollHeight) {
+    for (let i = 1; i < 2; i++) {
+      const listItem = document.createElement('li');
+      listItem.textContent = `List item ${list.children.length + i}`;
+      list.appendChild(listItem);
+    }
+  }
+});
